95f7e099ee06e6a03fcf0a68f5ffcc4b651f84c3
timeToCode-ali <time2code.ali@gmail.com>
2023-02-05 00:56:45 +0000

    updating config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
