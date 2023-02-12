a10393d8b96bcef0d8f0dde8927ec1853302987e
timeToCode-ali <time2code.ali@gmail.com>
2023-02-12 14:02:56 +0000

    adding data

---
+++ data/page-data.jsx
@@ -0,0 +1,66 @@
+export const personalData = {
+	name: "Joe Doe",
+	role: "Software Engineer",
+	education: [
+		"Bachelor of Science in Computer Science, University of Oxford(2015-2019)",
+		"Master of Science in Artificial Intelligence, Imperial College London (2019-2021)",
+	],
+	contactLinks: [
+		"mailto:emailto:time2code@gmail.com",
+		"https://twitter.com/time_tocode",
+		"https://linkedin.com/aliciarodriguezLi",
+		"https://medium.com/@time2code.ali",
+	],
+};
+
+export const aboutMe = {
+	title: "About Me",
+	body: [
+		"As a highly motivated and driven individual with a passion for making a positive impact, I have a unique blend of technical and interpersonal skills, thanks to my background in computer science. I am able to tackle complex problems with ease, and I never lose sight of my ultimate goal, even when facing challenges.",
+		"When I'm not working, I love hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
+	],
+};
+
+export const skills = {
+	soft: [
+		{ icon: "👂🏼", text: "Active Listening" },
+		{ icon: "💬", text: "Effective Communication" },
+		{ icon: "👥", text: "Collaboration" },
+		{ icon: "⽓", text: "Teamwork" },
+		{ icon: "💡", text: "Creative Problem Solving" },
+		{ icon: "⌛️", text: "Time management" },
+	],
+	hard: [
+		{ icon: "💻", text: "Python" },
+		{ icon: "ʦ", text: "TypeScript" },
+		{ icon: "🚀", text: "React" },
+		{ icon: "💾", text: "SQL" },
+		{ icon: "💾", text: "noSQL" },
+		{ icon: "📈", text: "Data Structures and Algorithms" },
+	],
+};
+
+export const professionalData = {
+	title: "Professional Experience",
+	experiences: [
+		{
+			role: "Data Scientist, Acme Inc. (2021-Present)",
+			description:
+				"Design and implement machine learning models to analyze large datasets and drive business decisions. Collaborate with cross-functional teams to develop and launch new products and features. Provide technical guidance and mentorship to junior data scientists. Conduct regular presentations to stakeholders on the findings and insights generated from data analysis.",
+			current: true,
+		},
+
+		{
+			role: "Machine Learning Engineer, XYZ Corp. (2019-2021)",
+			description:
+				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
+			current: false,
+		},
+		{
+			role: "Machine Learning Engineer, ABX Corp. (2020-2021)",
+			description:
+				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
+			current: false,
+		},
+	],
+};
