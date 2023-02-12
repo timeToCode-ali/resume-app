533fb8bd7c0be96885e6f49c555418ec1be4cff2
timeToCode-ali <time2code.ali@gmail.com>
2023-02-12 20:12:54 +0000

    Update professiona-experience.jsx
    missing key

--- app/component/professiona-experience.jsx
+++ app/component/professiona-experience.jsx
@@ -7,7 +7,7 @@ const ProfessionaExperience = ({ data }) => {
 			<h2 className='mb-6'> {title}</h2>
 			<div className='flex flex-col gap-6'>
 				{experiences.map(({ role, description, current }) => (
-					<div className='flex flex-col rounded-lg'>
+					<div key={role} className='flex flex-col rounded-lg'>
 						<span className={`h-2 ${current ? "bg-green" : "bg-grey"}`} />
 						<div className='bg-grey-light p-6 drop-shadow-md'>
 							<h3>{role}</h3>
